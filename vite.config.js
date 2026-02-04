74ecf097edd868e55a50bcace35261ff63305b52
Aleksei Chumachenko <aleksei.chumachenko@my.games>
2026-02-04 16:16:15 +0100

    deployment assets

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
+  base: '/longdivision/', 
   plugins: [react()],
-  base:'/longDivision'
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    cors: true
+  },
 })
