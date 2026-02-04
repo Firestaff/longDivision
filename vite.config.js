b42943980fd9917ef950eb507549e998391c6ebd
Aleksei Chumachenko <aleksei.chumachenko@my.games>
2026-02-04 17:27:25 +0100

    fix 1

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     outDir: 'docs',
     emptyOutDir: true,
   },
-  base: '/longdivision/', 
+  base: '/longDivision/docs/', 
   plugins: [react()],
   server: {
     host: '0.0.0.0',
